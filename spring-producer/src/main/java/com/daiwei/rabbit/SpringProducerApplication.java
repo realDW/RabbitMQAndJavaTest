03486178db84243a1bbd5f1c2a0be04166641c44
DW <m15879067251@163.com>
2018-10-17 18:09:32 +0800

    init

---
+++ spring-producer/src/main/java/com/daiwei/rabbit/SpringProducerApplication.java
@@ -0,0 +1,12 @@
+package com.daiwei.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProducerApplication.class, args);
+    }
+}
